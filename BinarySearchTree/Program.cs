8968dd2b9a6878fe24574094e93380395608a660
Chetan <Chetan@DESKTOP-9SU09BS>
2020-11-09 19:48:03 +0530

    [Chetan]Add. Added Elements to binary search tree.

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -11,7 +11,19 @@ namespace BinarySearchTree
             binarySearchTree.InsertData(56);
             binarySearchTree.InsertData(30);
             binarySearchTree.InsertData(70);
+            binarySearchTree.InsertData(22);
+            binarySearchTree.InsertData(40);
+            binarySearchTree.InsertData(11);
+            binarySearchTree.InsertData(3);
+            binarySearchTree.InsertData(16);
+            binarySearchTree.InsertData(60);
+            binarySearchTree.InsertData(95);
+            binarySearchTree.InsertData(65);
+            binarySearchTree.InsertData(63);
+            binarySearchTree.InsertData(67);
             binarySearchTree.Display(binarySearchTree.GetRoot());
+
+            Console.WriteLine("Count of elements in Binary search tree is: " + binarySearchTree.GetSize());
         }
     }
 }
