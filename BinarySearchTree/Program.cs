76d151738019410de823fc38b9cc6a1dda40b867
Chetan <Chetan@DESKTOP-9SU09BS>
2020-11-09 19:28:03 +0530

    [Chetan]Add. Create Binary search tree and add  elements.

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -7,6 +7,11 @@ namespace BinarySearchTree
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Binary Search Tree.");
+            BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>();
+            binarySearchTree.InsertData(56);
+            binarySearchTree.InsertData(30);
+            binarySearchTree.InsertData(70);
+            binarySearchTree.Display(binarySearchTree.GetRoot());
         }
     }
 }
